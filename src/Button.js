c8c000381f23614e0dcc4c2e01a7cf922cab2cfb
Daniel Segura <danielseguraf@gmail.com>
2020-03-10 20:15:30 -0500

    border and font size buttons

--- src/Button.js
+++ src/Button.js
@@ -33,8 +33,8 @@ const SIZES = {
 };
 
 const StyledButton = styled.button`
-  border: 10px solid red;
-  font-size: 20px;
+  border: 0;
+  font-size: 14px;
   border-radius: 3em;
   cursor: pointer;
   display: inline-block;
